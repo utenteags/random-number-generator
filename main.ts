d32158b6e860a5a75b3d6ec0e45641bb6b3230c7
bigjohnson <alberto@panu.it>
2024-08-07 14:24:10 +0200

    add better gui

--- main.ts
+++ main.ts
@@ -45,3 +45,37 @@ for (const ruota of ruote) {
 console.log(JSON.stringify(estrazioni, null, 2));
 
 //console.log(RNGDec(0, 10, 2.2));
+
+function createRuotaCnt(ruotaName: string, estrazioni: number[]) {
+    const ruotaDiv = document.createElement('div');
+    ruotaDiv.className = 'ruota ' + ruotaName.toLowerCase();
+    const nameH2 = document.createElement('h2');
+    nameH2.innerText = ruotaName;
+    nameH2.className = 'ruota-title'
+    ruotaDiv.appendChild(nameH2);
+
+    for (const num of estrazioni) {
+        const numP = document.createElement('p');
+        numP.innerText = "" +num;
+        const numDiv = document.createElement('div');
+        numDiv.className = 'ruota-estrazione'
+        numDiv.appendChild(numP);
+        ruotaDiv.appendChild(numDiv);
+    }
+
+    return ruotaDiv;
+}
+
+const container = document.getElementById('cnt');
+if (container) {
+    const pre = document.createElement('pre');
+    //pre.innerText = JSON.stringify(estrazioni, null, 2);
+    //container.appendChild(pre);
+
+    for (const ruota of ruote) {
+        const ruotaEstrazioni = estrazioni[ruota];
+        const ruotaDiv = createRuotaCnt(ruota, ruotaEstrazioni);
+        container.appendChild(ruotaDiv);
+    }
+}
+
